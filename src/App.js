1115c290d0ca351843061b42326f3d39ff2270ab
CharaeKeow <charae.ehsin@gmail.com>
2022-08-02 13:06:59 +0800

    Add static date

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { format } from 'date-fns'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Today is {format(new Date(), "'Today is a' eeee yyyy-MM-dd HH:mm:ss")}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
